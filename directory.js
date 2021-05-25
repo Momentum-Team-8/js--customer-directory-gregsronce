2828b752094e90036cf1117e0970838979298b59
Greg Sronce <greg.sronce@gmail.com>
2021-05-25 17:09:58 -0400

    Made initial divs for directory members.

---
+++ directory.js
@@ -0,0 +1,65 @@
+// divs for each member of the directory
+
+let divHeader = document.createElement('div');
+divHeader.innerHTML = '<h1>Customer Directory</h1>';
+document.getElementById('header').appendChild(divHeader);
+
+let divSophia = document.createElement('div');
+divSophia.className = 'info';
+divSophia.id = 'Sophia';
+document.getElementById('output').appendChild(divSophia);
+
+let divKrin = document.createElement('div');
+divKrin.className = 'info';
+divKrin.id = 'Krin';
+document.getElementById('output').appendChild(divKrin);
+
+let divLetitia = document.createElement('div');
+divLetitia.className = 'info';
+divLetitia.id = 'Letitia';
+document.getElementById('output').appendChild(divLetitia);
+
+let divMargie = document.createElement('div');
+divMargie.className = 'info';
+divMargie.id = 'Margie';
+document.getElementById('output').appendChild(divMargie);
+
+let divCristina = document.createElement('div');
+divCristina.className = 'info';
+divCristina.id = 'Cristina';
+document.getElementById('output').appendChild(divCristina);
+
+let divLaurie = document.createElement('div');
+divLaurie.className = 'info';
+divLaurie.id = 'Laurie';
+document.getElementById('output').appendChild(divLaurie);
+
+let divRussell = document.createElement('div');
+divRussell.className = 'info';
+divRussell.id = 'Russell';
+document.getElementById('output').appendChild(divRussell);
+
+let divCarmen = document.createElement('div');
+divCarmen.className = 'info';
+divCarmen.id = 'Carmen';
+document.getElementById('output').appendChild(divCarmen);
+
+let divErika = document.createElement('div');
+divErika.className = 'info';
+divErika.id = 'Erika';
+document.getElementById('output').appendChild(divErika);
+
+let divClifford = document.createElement('div');
+divClifford.className = 'info';
+divClifford.id = 'Clifford';
+document.getElementById('output').appendChild(divClifford);
+
+let divCody = document.createElement('div');
+divCody.className = 'info';
+divCody.id = 'Cody';
+document.getElementById('output').appendChild(divCody);
+
+let divMiguel = document.createElement('div');
+divMiguel.className = 'info';
+divMiguel.id = 'Miguel';
+document.getElementById('output').appendChild(divMiguel);
