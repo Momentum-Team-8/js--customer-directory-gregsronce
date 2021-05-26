6cfc3c85dc9ab3f7b0755047a21e6fe7eb072305
Greg Sronce <greg.sronce@gmail.com>
2021-05-25 21:09:43 -0400

    First draft of all files with info. Needs final touches.

--- directory.js
+++ directory.js
@@ -1,65 +1,40 @@
-// divs for each member of the directory
-
+// header div
 let divHeader = document.createElement('div');
 divHeader.innerHTML = '<h1>Customer Directory</h1>';
 document.getElementById('header').appendChild(divHeader);
 
-let divSophia = document.createElement('div');
-divSophia.className = 'info';
-divSophia.id = 'Sophia';
-document.getElementById('output').appendChild(divSophia);
+// loop to create page
+const customer = customers[0]
+const insertionPoint = document.querySelector('#output')
 
-let divKrin = document.createElement('div');
-divKrin.className = 'info';
-divKrin.id = 'Krin';
-document.getElementById('output').appendChild(divKrin);
+for (let customer of customers) {
+    const customerCard = document.createElement('div')
+    customerCard.classList.add('customer-card')
+    insertionPoint.appendChild(customerCard)
+    console.log(customerCard)
+    const customerImage = document.createElement('img')
+    customerImage.src = customer.picture.large
+    customerCard.appendChild(customerImage)
+    const customerName = document.createElement('div')
+    customerName.innerText = customer.name.first + " " + customer.name.last 
+    customerCard.appendChild(customerName)
+    const customerEmail = document.createElement('div')
+    customerEmail.innerText = customer.email
+    customerCard.appendChild(customerEmail)
+    const customerAddress = document.createElement('div')
+    customerAddress.innerText = customer.location.street.number + " " + customer.location.street.name
+    customerCard.appendChild(customerAddress)
+    const customerTown = document.createElement('div')
+    customerTown.innerText = customer.location.city + ", " + " " + customer.location.state + " " + customer.location.postcode
+    customerCard.appendChild(customerTown)
+    const dateOfBirth = document.createElement ('div')
+    dateOfBirth.innerText = "DOB" + " " + customer.dob.date
+    customerCard.appendChild(dateOfBirth)
+    const customerRegistration = document.createElement('div')
+    customerRegistration.innerText = "Customer since:" + " " + customer.registered.date
+    customerCard.appendChild(customerRegistration)
+}
 
-let divLetitia = document.createElement('div');
-divLetitia.className = 'info';
-divLetitia.id = 'Letitia';
-document.getElementById('output').appendChild(divLetitia);
 
-let divMargie = document.createElement('div');
-divMargie.className = 'info';
-divMargie.id = 'Margie';
-document.getElementById('output').appendChild(divMargie);
 
-let divCristina = document.createElement('div');
-divCristina.className = 'info';
-divCristina.id = 'Cristina';
-document.getElementById('output').appendChild(divCristina);
 
-let divLaurie = document.createElement('div');
-divLaurie.className = 'info';
-divLaurie.id = 'Laurie';
-document.getElementById('output').appendChild(divLaurie);
-
-let divRussell = document.createElement('div');
-divRussell.className = 'info';
-divRussell.id = 'Russell';
-document.getElementById('output').appendChild(divRussell);
-
-let divCarmen = document.createElement('div');
-divCarmen.className = 'info';
-divCarmen.id = 'Carmen';
-document.getElementById('output').appendChild(divCarmen);
-
-let divErika = document.createElement('div');
-divErika.className = 'info';
-divErika.id = 'Erika';
-document.getElementById('output').appendChild(divErika);
-
-let divClifford = document.createElement('div');
-divClifford.className = 'info';
-divClifford.id = 'Clifford';
-document.getElementById('output').appendChild(divClifford);
-
-let divCody = document.createElement('div');
-divCody.className = 'info';
-divCody.id = 'Cody';
-document.getElementById('output').appendChild(divCody);
-
-let divMiguel = document.createElement('div');
-divMiguel.className = 'info';
-divMiguel.id = 'Miguel';
-document.getElementById('output').appendChild(divMiguel);
